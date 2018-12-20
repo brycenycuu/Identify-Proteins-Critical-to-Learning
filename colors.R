3c0f4f55a5bf67c732b828b71041dbf8a161967d
Di Wu <dw2834@columbia.edu>
2018-12-20 14:05:44 -0500

    Add files via upload

---
+++ colors.R
@@ -0,0 +1,13 @@
+# colors
+my_colors <- c(
+  'c-CS-s' = "#88e175",
+  'c-CS-m' = "#fbe44d",
+  'c-SC-s' = "#bf6a26",
+  'c-SC-m' = "#f19c38",
+  't-CS-s' = "#f5bf43",
+  't-CS-m' = "#ec5864",
+  't-SC-s' = "#a8dae6",
+  't-SC-m' = "#518aea",
+  'none' = "#e6e6e6",
+  'none1' = "#e6e6e6"
+)
